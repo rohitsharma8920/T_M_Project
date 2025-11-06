8bbc6ef23002bf749a799037810eff3ad900556a
rohitsharma8920 <rsharmaji7976@gmail.com>
2025-11-06 12:09:07 +0530

    Update utils.js

--- frontend/src/utils.js
+++ frontend/src/utils.js
@@ -4,7 +4,7 @@ export const notify = (message, type) => {
     toast[type](message);
 }
 
-export const API_URL = 'http://localhost:3000';
+export const API_URL = 'https://t-m-project-api.vercel.app/';
 
 
 // export const API_URL = 'https://mern-task-manager-app-deploy-api.vercel.app';
